786c08c93b9b6bb81ff57efd6d618c98f4b51f63
mukundhasuresh <mukundhasuresh@gmail.com>
2026-02-24 21:44:59 +0530

    Updated with cookie based JWT

--- src/app.js
+++ src/app.js
@@ -1,16 +1,34 @@
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // frontend URL
+    credentials: true, // IMPORTANT for cookies
+  })
+);
+
 app.use(express.json());
+app.use(cookieParser());
 
 // Routes
 app.use("/api/auth", authRoutes);
+
+// Temporary protected test route
+const { protect } = require("./middleware/authMiddleware");
+
+app.get("/api/protected", protect, (req, res) => {
+  res.json({
+    message: "Secure route accessed",
+    user: req.user,
+  });
+});
 
 app.get("/", (req, res) => {
   res.send("SecurePipeline API running...");
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,9 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+
+// REGISTER
 exports.register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -29,4 +32,51 @@ exports.register = async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
+};
+
+
+// LOGIN → COOKIE JWT
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    // 🔥 Send token as HTTP-only cookie
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false, // 👉 change to true in production (HTTPS)
+      sameSite: "lax",
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    res.json({
+      message: "Login successful",
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+
+// ✅ LOGOUT
+exports.logout = (req, res) => {
+  res.clearCookie("token");
+  res.json({ message: "Logged out successfully" });
 };
