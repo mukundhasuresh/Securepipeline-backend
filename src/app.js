6ad18b6f3581b58b19d9ed62cb3033daba7c10ef
mukundhasuresh <mukundhasuresh@gmail.com>
2026-02-24 20:32:32 +0530

    Auth routes added

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("SecurePipeline API running...");
